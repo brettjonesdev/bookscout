6accb9fb571696ddaf31cb2fce70fee38b034292
Gmajoulet <gmajoulet@gmail.com>
2015-04-26 18:34:39 +0200

    Display as thumbnails or grid

--- app/controllers/bookshelf.js
+++ app/controllers/bookshelf.js
@@ -1,5 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  sortProperties: ['title', 'author']
+  sortProperties: ['title', 'author'],
+  queryParams: ['display'],
+
+  /**
+   * Should display results as a grid?
+   *
+   * @return {Boolean}
+   */
+  shouldDisplayGrid: function () {
+    var display = this.get('display');
+
+    // If display is not set or equal to grid
+    return !display || 'grid' === this.get('display');
+  }.property('display'),
+
+  /**
+   * Should display results as thumbnails?
+   *
+   * @return {Boolean}
+   */
+  shouldDisplayThumbnail: function () {
+    return 'thumbnail' === this.get('display');
+  }.property('display')
 });
