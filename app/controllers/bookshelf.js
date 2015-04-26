994cf6f9d1c7045fbe183008a39cb35d401b3fdd
Gmajoulet <gmajoulet@gmail.com>
2015-04-26 19:15:00 +0200

    Switching displays

--- app/controllers/bookshelf.js
+++ app/controllers/bookshelf.js
@@ -3,6 +3,23 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   sortProperties: ['title', 'author'],
   queryParams: ['display'],
+  display: 'grid',
+
+  /**
+   * Sets the new display when the user clicks on the switch
+   */
+  changeDisplay: function () {
+    this.set('display', this.get('displaySwitch') ? 'thumbnail' : 'grid');
+  }.observes('displaySwitch'),
+
+  /**
+   * Initialize the switch value
+   *
+   * @return {Boolean}
+   */
+  displaySwitch: function () {
+    return 'thumbnail' === this.get('display');
+  }.property(),
 
   /**
    * Should display results as a grid?
